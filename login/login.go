33f89a726bf7b946c354127e6f37b547041b354b
Ian <git@loosescre.ws>
2015-01-10 22:00:20 -0800

    Added  sign-up-and-log-in class to tabs.

--- login/login.go
+++ login/login.go
@@ -78,7 +78,7 @@ const loginTemplateText = `
 				<div class="alert-box alert">
 					{{.formError}}
 				</div>{{end}}
-				<section class="login-tabs">
+				<section class="login-tabs sign-up-and-log-in">
 					<dl class="tabs" data-tab>
 						<dd{{if not .loginSelected}} class="active"{{end}}><a href="#sign-up-form">Sign Up</a></dd>
 						<dd{{if .loginSelected}} class="active"{{end}}><a href="#log-in-form">Log In</a></dd>
