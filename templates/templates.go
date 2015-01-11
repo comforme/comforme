ad80ebe126eb78f0240795aed4701578a7e272ca
SpyroSoft <meta@spyrosoft.com>
2015-01-11 12:17:52 -0800

    Swap out the page search with the communities search.

--- templates/templates.go
+++ templates/templates.go
@@ -62,7 +62,16 @@ const SearchBar = `
 `
 
 const CommunitySearch = `
-    {{template "searchBar" . }}
+    <form action="COMMUNITIES-SEARCH-FORM-ACTION-REPLACE-ME" method="post">
+        <div class="row collapse">
+            <div class="small-10 columns">
+                <input type="text" placeholder="Communities Search" name="communities-search" id="communities-search-textbox">
+            </div>
+            <div class="small-2 columns">
+                <button type="submit" class="button postfix">Search</button>
+            </div>
+        </div>
+    </form>
     <form action="USER-ADD-COMMUNITIES-FORM-ACTION-REPLACE-ME" method="post">
         <div class="row">
             <div class="large-6 medium-6 small-12 columns left">
