714962a8f8177bd670f50e6a17ccdb782becea8a
peter Czupil <pczupil@ucsc.edu>
2015-01-10 19:09:10 -0800

    home page is working but just has a placeholder

--- home/home.go
+++ home/home.go
@@ -11,6 +11,28 @@ func HomeHandler(res http.ResponseWriter, req *http.Request) {
 	data := map[string]interface{}{}
 
 	// TODO: Add template and compile it.
-	tmpl, _ := template.New("test").ParseFiles("/templates/templates.go")
+	tmpl, _ := template.New("test").Parse(rootPageTemplateHtml)
 	common.ExecTemplate(tmpl, res, data)
 }
+
+//Generic page template
+
+const rootPageTemplateHtml = `
+<!DOCTYPE html>
+<html>
+  <head>
+	<title>{{.PageTitle}}</title>
+	<style type="text/css" media="all">
+		<!--
+			.your_entry { background: yellow; }
+			.other_entry { background: #00FFFF; }
+			.errortext { color: red; font-weight: bold }
+			body { font-family: Helvetica, Arial, Sans-Serif; color: red }
+		-->
+	</style>
+  </head>
+  <body>
+	I really love Wine!
+  </body>
+</html>
+`
