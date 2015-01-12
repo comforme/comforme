4048f7141c11326b8a5bcfaf212da39fb3460001
Ian <git@loosescre.ws>
2015-01-12 00:29:58 -0800

    Formatting.

--- templates/templates.go
+++ templates/templates.go
@@ -37,17 +37,17 @@ const NavBar = `
 			</ul>
 			<ul class="right">
 				<li>
-				<a href="/" title="Main Page"><i class="fi-home"><span class="show-for-small-only"> Main Page</span></i></a>
-			</li>
-			<li>
-				<a href="/newPage" title="Add Page"><i class="fi-page-add"><span class="show-for-small-only"> Add Page</span></i></a>
-			</li>
-			<li>
-				<a href="/settings" title="Settings"><i class="fi-widget"><span class="show-for-small-only"> Settings</span></i></a>
-			</li>
-			<li>
-				<a href="/logout" title="Log Out"><i class="fi-power"><span class="show-for-small-only"> Logout</span></i></a>
-			</li>
+					<a href="/" title="Main Page"><i class="fi-home"><span class="show-for-small-only"> Main Page</span></i></a>
+				</li>
+				<li>
+					<a href="/newPage" title="Add Page"><i class="fi-page-add"><span class="show-for-small-only"> Add Page</span></i></a>
+				</li>
+				<li>
+					<a href="/settings" title="Settings"><i class="fi-widget"><span class="show-for-small-only"> Settings</span></i></a>
+				</li>
+				<li>
+					<a href="/logout" title="Log Out"><i class="fi-power"><span class="show-for-small-only"> Logout</span></i></a>
+				</li>
 			</ul>
 		</section>
 	</nav>
