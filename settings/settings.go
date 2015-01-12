bfde1a446a0c5497a1a7f36d7ce8f053db9812d5
Ian <git@loosescre.ws>
2015-01-12 00:38:48 -0800

    Added h6 tags around checking instructions.

--- settings/settings.go
+++ settings/settings.go
@@ -143,7 +143,7 @@ const settingsTemplateText = `
 				</section>
 				<section>
 					<h2>Your Communities</h2>
-					Check all that apply.
+					<h6>Check all that apply.</h6>
 					<div class="row">{{range $col_number, $communitiesCol := $.communitiesCols}}
 						<div class="large-3 medium-6 small-12 columns left">{{range $line_number, $community := $communitiesCol}}
 							<div>
