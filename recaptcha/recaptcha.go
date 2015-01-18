ada883ced94997e3fffe80e55c48160e74eb5b44
Raymond Colebaugh <raymondcolebaugh@gmail.com>
2015-01-17 20:30:15 -0800

    Added recaptcha GET API call to verify match

--- recaptcha/recaptcha.go
+++ recaptcha/recaptcha.go
@@ -3,6 +3,14 @@ package recaptcha
 // reCaptcha Documentation:
 // https://developers.google.com/recaptcha/docs/verify
 
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
 var secret string
 
 type recaptchaResult struct {
@@ -15,5 +23,25 @@ func Init(newSecret string) {
 }
 
 func Check(response, remoteip string) error {
-	return nil
+	apiEndpoint := "https://www.google.com/recaptcha/api/siteverify"
+	params := fmt.Sprintf("?secret=%s&response=%s&remoteip=%s",
+		secret,
+		response,
+		remoteip)
+	resp, err := http.Get(apiEndpoint + params)
+	defer resp.Body.Close()
+	if err == nil {
+		body, err := ioutil.ReadAll(resp.Body)
+		if err == nil {
+			var data recaptchaResult
+			json.Unmarshal(body, &data)
+			if data.Success {
+				return nil
+			}
+			err := errors.New(data.Error)
+			return err
+		}
+		return err
+	}
+	return err
 }
