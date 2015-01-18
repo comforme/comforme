01dd2e88378e931803689cc25d97e9cceba4c580
Ian <git@loosescre.ws>
2015-01-17 17:23:02 -0800

    Added recaptchaResult.

--- recaptcha/recaptcha.go
+++ recaptcha/recaptcha.go
@@ -6,6 +6,11 @@ import(
 
 var secret string
 
+type struct recaptchaResult {
+	Success bool `json:"success"`
+	Error string `json:"error-codes"`
+}
+
 func Init(newSecret string) {
 	secret = newSecret
 }
