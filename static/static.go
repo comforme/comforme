b7fd9b1fc53f489c3aaab310ea571b1359e23226
Ian <git@loosescre.ws>
2015-05-19 21:50:50 -0700

    Added logging and ./ for serving static files.

--- static/static.go
+++ static/static.go
@@ -1,11 +1,14 @@
 package static
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
 )
 
 func StaticHandler(res http.ResponseWriter, req *http.Request, ps httprouter.Params) {
-	http.ServeFile(res, req, "staticFiles/"+ps.ByName("filepath"))
+	path := "./staticFiles/" + ps.ByName("filepath")
+	log.Println("Serving file:", path)
+	http.ServeFile(res, req, path)
 }
