d255e4aa3c35b09a75f78b3149244596bd2e8e40
Ian <git@loosescre.ws>
2015-05-19 20:05:59 -0700

    Added tabs to onboarding process.

--- wizard/wizard.go
+++ wizard/wizard.go
@@ -204,8 +204,42 @@ const wizardTemplateText = `
 `
 
 const communitiesTemplateText = `
-					{{ template "communitiesContent" .}}
-					<button onclick="location.href='/'">Finish</button>
+					<ul class="tabs small-block-grid-2 medium-block-grid-3 xlarge-block-grid-6" data-tab role="tablist">
+						<li class="tab-title active" role="presentational" ><a href="#tab-communities" role="tab" tabindex="0" aria-selected="false" controls="tab-communities" id="tab-communities-navigation">Your Communities</a></li>
+						<li class="tab-title" role="presentational" ><a href="#tab-nav-bar" role="tab" tabindex="0" aria-selected="true" controls="tab-nav-bar" id="tab-nav-bar-navigation">The Nav Bar</a></li>
+						<li class="tab-title" role="presentational"><a href="#tab-finding-resources" role="tab" tabindex="0" aria-selected="false" controls="tab-finding-resources" id="tab-finding-resources-navigation">Finding Resources</a></li>
+						<li class="tab-title" role="presentational" ><a href="#tab-reviews" role="tab" tabindex="0" aria-selected="false" controls="tab-reviews" id="tab-reviews-navigation">Reviews</a></li>
+						<li class="tab-title" role="presentational" ><a href="#tab-adding-resources" role="tab" tabindex="0" aria-selected="false" controls="tab-adding-resources" id="tab-adding-resources-navigation">Adding Resources</a></li>
+						<li class="tab-title" role="presentational" ><a href="#tab-settings" role="tab" tabindex="0" aria-selected="false" controls="tab-settings" id="tab-settings-navigation">Changing Settings</a></li>
+					</ul>
+					<div class="tabs-content">
+						<section role="tabtab" aria-hidden="false" class="content active" id="tab-communities">
+							<h2>Join Communities</h2>
+							{{ template "communitiesContent" .}}
+							<button onClick="location.href='#tab-nav-bar'">Next</button>
+						</section>
+						<section role="tabtab" aria-hidden="true" class="content" id="tab-nav-bar">
+							<h2>The Nav Bar</h2>
+							<p>The nav bar is at the top of every page when you are logged in.</p>
+							<button onClick="location.href='#tab-finding-resources'">Next</button>
+						</section>
+						<section role="tabtab" aria-hidden="true" class="content" id="tab-finding-resources">
+							<h2>Finding Resources</h2>
+							<button onClick="location.href='#tab-reviews'">Next</button>
+						</section>
+						<section role="tabtab" aria-hidden="true" class="content" id="tab-reviews">
+							<h2>Reviews</h2>
+							<button onClick="location.href='#tab-adding-resources'">Next</button>
+						</section>
+						<section role="tabtab" aria-hidden="true" class="content" id="tab-adding-resources">
+							<h2>Adding Resources</h2>
+							<button onClick="location.href='#tab-settings'">Next</button>
+						</section>
+						<section role="tabtab" aria-hidden="true" class="content" id="tab-settings">
+							<h2>Changing Settings</h2>
+							<button onclick="location.href='/'">Finish</button>
+						</section>
+					</div>
 				`
 
 const registerTemplateText = `
