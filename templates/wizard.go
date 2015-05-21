27e52663091282f1f405dfe002b558133543c558
Ian <git@loosescre.ws>
2015-05-20 18:59:13 -0700

    Changed Tour to Tour Wizard

--- templates/wizard.go
+++ templates/wizard.go
@@ -4,7 +4,7 @@ const Wizard = `
 	<div class="content">
 		<div class="row">
 			<div class="columns communities-settings">
-				<h1><i class="fi-foundation"></i> Tour</h1>
+				<h1><i class="fi-foundation"></i> Tour Wizard</h1>
 				{{if .successMsg}}<div class="alert-box success">{{.successMsg}}</div>{{end}}
 				{{if .errorMsg}}<div class="alert-box alert">{{.errorMsg}}</div>{{end}}
 				<section>{{ template "wizardContent" .}}</section>
