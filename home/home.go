1870d4718022b8b6bdf72557195e4291df1dce6e
Ian <git@loosescre.ws>
2015-05-20 18:46:31 -0700

    Reordered home page items.

--- home/home.go
+++ home/home.go
@@ -48,15 +48,15 @@ const homeTemplateText = `
 	</div>
 	<div class="row">
 		<div class="columns left">
+			Lost? <a href="/wizard">Take the tour</a> again.
+		</div>
+	</div>
+	<div class="row">
+		<div class="columns left">
 			<h2>Top Resources:</h2>
 		</div>{{range .topPages}}
 		<div class="columns left large-3 medium-4 small-6 xsmall-12">
 			<h3><a href="/page/{{.CategorySlug}}/{{.PageSlug}}">{{.Title}}</a></h3>
 		</div>{{ end }}
-	</div>
-	<div class="row">
-		<div class="columns left">
-			Lost? <a href="/wizard">Take the tour</a> again.
-		</div>
 	</div>
 </div>`
